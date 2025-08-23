21fdd494dfedc613025e7a4082cfa1bd0a3ec934
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-08-23 16:39:45 -0300

    Seed Database

--- Infra/Db/AppDbContext.cs
+++ Infra/Db/AppDbContext.cs
@@ -5,11 +5,21 @@ namespace minimal_api.Infra.Db
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Admin> Administradores { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>().HasData(
+                new Admin()
+                {
+                    Id = 1,
+                    Email = "administrador@teste.com",
+                    Senha = "123456",
+                    Role = "Admin"
+                }
+            );
+        }
     }
 }
